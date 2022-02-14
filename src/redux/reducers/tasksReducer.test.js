bf56733108364062ba91962e2bcf2f8bcf392c77
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-14 23:30:30 +0100

    Added test to deleteTask in tasksReducer.js

--- src/redux/reducers/tasksReducer.test.js
+++ src/redux/reducers/tasksReducer.test.js
@@ -2,7 +2,7 @@ import actionsTypes from "../actions/actionsTypes";
 import tasksReducer from "./tasksReducer";
 
 describe("Given a tasksReducer function", () => {
-  describe("When it receives currentTasks and an action with tasks 'Do something' and 'Do something else'", () => {
+  describe("When it receives currentTasks and loadTasks action with tasks 'Do something' and 'Do something else'", () => {
     test("Then it should return tasks 'Do something' and 'Do something else'", () => {
       const currentTasks = [];
 
@@ -27,6 +27,42 @@ describe("Given a tasksReducer function", () => {
       const reducerResult = tasksReducer(currentTasks, action);
 
       expect(reducerResult).toEqual(tasks);
+    });
+  });
+
+  describe("When it receives currentTasks with 'Do something' & 'Do something else', and deleteTask action with id 2", () => {
+    test("Then it should return task 'Do something''", () => {
+      const currentTasks = [
+        {
+          name: "Do something",
+          done: "true",
+          id: 1,
+        },
+        {
+          name: "Do something else",
+          done: "false",
+          id: 2,
+        },
+      ];
+
+      const id = 2;
+
+      const action = {
+        type: actionsTypes.deleteTask,
+        id,
+      };
+
+      const expectedTask = [
+        {
+          name: "Do something",
+          done: "true",
+          id: 1,
+        },
+      ];
+
+      const reducerResult = tasksReducer(currentTasks, action);
+
+      expect(reducerResult).toEqual(expectedTask);
     });
   });
 
