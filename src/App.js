30aa214b21cf82f0abac7ae3f26b2bf7801cd833
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-14 20:35:30 +0100

    Renderd Task in App.js and added styles to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+import Task from "./components/Task/Task";
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid red;
+  width: 70vw;
+`;
+
+const ToDoList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0;
+  padding: 0;
+  border: 1px solid yellowgreen;
+`;
+
 function App() {
-  return <></>;
+  const toDo = [
+    {
+      id: 1,
+      name: "Do something",
+      done: true,
+    },
+    {
+      id: 2,
+      name: "Do something else",
+      done: false,
+    },
+  ];
+
+  return (
+    <>
+      <Section>
+        <ToDoList>
+          {toDo.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </ToDoList>
+      </Section>
+    </>
+  );
 }
 
 export default App;
