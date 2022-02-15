3e4eca72b8627107b72f3483f5c2500e0741b8de
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-15 21:29:51 +0100

    Added test to createTaskThunk function

--- src/redux/thunks/tasksThunks.test.js
+++ src/redux/thunks/tasksThunks.test.js
@@ -1,4 +1,8 @@
-import { deleteTaskThunk, loadTasksThunk } from "./tasksThunks";
+import {
+  createTaskThunk,
+  deleteTaskThunk,
+  loadTasksThunk,
+} from "./tasksThunks";
 
 describe("Given a loadTasksThunk function", () => {
   describe("When it is called", () => {
@@ -36,6 +40,25 @@ describe("Given a deleteTaskThunk inner function", () => {
       await deleteThunk(dispatch);
 
       expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a createTaskThunk function", () => {
+  describe("When it is called with a task", () => {
+    test("Then it should dispatch a function", async () => {
+      const dispatch = jest.fn();
+      const task = {
+        name: "Do something please",
+        done: "false",
+        id: 3,
+      };
+
+      const createTask = createTaskThunk(task);
+
+      await createTask(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
