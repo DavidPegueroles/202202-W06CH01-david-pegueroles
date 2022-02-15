36967fa9a81720adddf5aabe07749268c3d63fc5
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-15 21:28:41 +0100

    Added test to createTask reducer

--- src/redux/reducers/tasksReducer.test.js
+++ src/redux/reducers/tasksReducer.test.js
@@ -66,6 +66,38 @@ describe("Given a tasksReducer function", () => {
     });
   });
 
+  describe("When it receives currentTasks with 'Do something' & 'Do something else', and createTask action with 'Do something please'", () => {
+    test("Then it should return the three tasks", () => {
+      const currentTasks = [
+        {
+          name: "Do something",
+          done: "true",
+          id: 1,
+        },
+        {
+          name: "Do something else",
+          done: "false",
+          id: 2,
+        },
+      ];
+
+      const task = {
+        name: "Do something please",
+        done: "false",
+        id: 3,
+      };
+
+      const action = {
+        type: actionsTypes.createTask,
+        task,
+      };
+
+      const reducerResult = tasksReducer(currentTasks, action);
+
+      expect(reducerResult).toHaveLength(3);
+    });
+  });
+
   describe("When it receives currentTasks and a non existing action with tasks 'Do something' and 'Do something else'", () => {
     test("Then it should return currentTasks", () => {
       const currentTasks = [];
