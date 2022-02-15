cf8af7d6d9b927d005a5275236f119c205bc4e05
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-15 02:07:34 +0100

    Added test in app.js to test async functions

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
+import renderWithProviders from "./setupTests";
 
 describe("Given an App component", () => {
   describe("When it's rendered", () => {
@@ -15,6 +16,20 @@ describe("Given an App component", () => {
       const linkElement = screen.getByRole("heading", { name: /todo list/i });
 
       expect(linkElement).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Given a TaskListComponent", () => {
+  describe("When it receives 'Do something' and 'Do something else' tasks", () => {
+    test("Then it should display 'Do something' and 'Do something else' inside", async () => {
+      renderWithProviders(<App />);
+
+      const expectedFirstTask = await screen.findByText("Do something");
+      const expectedSecondTask = await screen.findByText("Do something else");
+
+      expect(expectedFirstTask).toBeInTheDocument();
+      expect(expectedSecondTask).toBeInTheDocument();
     });
   });
 });
